eb42718deab5553f77470f61f3a47a925ca7356a
Honor <55959605+HonoR974@users.noreply.github.com>
2024-09-16 12:55:32 +0200

    Dynamisation Menu

--- js/test.js
+++ js/test.js
@@ -1,45 +1,51 @@
-let linkMenu = new Map();
-linkMenu.set("Accueil", "index.html");
-linkMenu.set("Produits", "productList.html");
-linkMenu.set("Logs", "log.html");
+// Fonction pour générer le menu
+function createMenu() {
+  // Le code HTML du menu
+  const menuHTML = `
+      <!-- Menu -->
+      <header>
+          <nav>
+              <div class="iconSide">
+                  <a href="index.html">
+                      <img class="icon" src="../assets/icon/logo.png" alt="logo icon">
+                  </a>
+              </div>
+              <div class="link">
+                  <ul>
+                      <li><a href="index.html">Accueil</a></li>
+                      <li><a href="productList.html">Produits</a></li>
+                  </ul>
+              </div>
+              <div>
+                  <form class="d-flex" role="search">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                      <button class="btn btn-outline-success" type="submit">Search</button>
+                  </form>
+              </div>
+              <div id="log">
+                  <a href="log.html">
+                      <img class="icon" src="../assets/icon/login.png" alt="login icon">
+                  </a>
+              </div>
+          </nav>
+      </header>
+  `;
 
-const iconPath = "../assets/icon/logo.png";
+  // Sélectionner l'élément dans lequel le menu sera inséré
+  const menuContainer = document.getElementById("menu-container");
 
-document.addEventListener("DOMContentLoaded", function () {
-
-/**
- * 
- * for (let [key, value] of map) {
-    console.log(key, value);
+  // Insérer le menu dans le conteneur
+  menuContainer.innerHTML = menuHTML;
 }
- */
 
-  const header = document.getElementsByTagName("header");
+// Attendre le chargement complet de la page pour exécuter la fonction
+window.addEventListener("DOMContentLoaded", createMenu);
 
-  const iconSide = document.createElement("div");
-  iconSide.className = "icon-side";
-
-  const aSide = document.createElement("a");
-  aSide.href = "index.html";
-
-  const icon = document.createElement("img");
-  icon.className = "icon";
-  icon.src = "../assets/icon/logo.png";
-
-  aSide.appendChild(icon);
-  iconSide.appendChild(aSide);
-  header.appendChild(iconSide);
-  
-});
-
-
-
-const iconSide = document.querySelector(".iconSide");
-const link = document.querySelector(".link");
-
-showMenuIn3Sec();
+window.addEventListener("DOMContentLoaded", showMenuIn3Sec);
 
 function showMenuIn3Sec() {
+  const iconSide = document.querySelector(".iconSide");
+  const link = document.querySelector(".link");
   let index = 0;
   for (let i = 0; i < 7; i++) {
     setTimeout(() => {
