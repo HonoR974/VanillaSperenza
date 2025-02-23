96bd9c7427d0a0da9fd1070ab009c821c9d1680f
honoR974 <honore.guillaudeau1@gmail.com>
2025-02-23 19:32:42 +0100

    Design et fonctionnalité de la ligne produit sur la panier
    - la page panier recharge les articles a chaque fois qu'elle est appelé
    - La croix dans ligne de panier doit permettre de supprimer l'article en question

--- js/panier.js
+++ js/panier.js
@@ -6,105 +6,186 @@ const sidebar = document.getElementById("sidebar");
 
 // Ouvrir la sidebar
 opentBtnPanier.addEventListener("click", () => {
-    sidebarContainer.classList.add("active");
-    sidebar.classList.add("active");
-    getPanierContent(); 
-  });
+  sidebarContainer.classList.add("active");
+  sidebar.classList.add("active");
+  getPanierContent();
+});
 
 // Fermer la sidebar
 closeBtn.addEventListener("click", () => {
-    sidebarContainer.classList.remove("active");
-    sidebar.classList.remove("active");
-    deletePanierContent(); 
+  sidebarContainer.classList.remove("active");
+  sidebar.classList.remove("active");
+  deletePanierContent();
 });
 
 // Fermer en cliquant en dehors du panier
 sidebarContainer.addEventListener("click", (event) => {
-    if (event.target === sidebarContainer) { // Vérifie si on clique en dehors de la sidebar
-        sidebarContainer.classList.remove("active");
-        sidebar.classList.remove("active");
-        deletePanierContent(); 
-    }
+  if (event.target === sidebarContainer) {
+    // Vérifie si on clique en dehors de la sidebar
+    sidebarContainer.classList.remove("active");
+    sidebar.classList.remove("active");
+    deletePanierContent();
+  }
 });
 
 class Product {
-    id; 
-    name; 
-    prix; 
-    quantite; 
-    pathImage; 
+  id;
+  name;
+  prix;
+  quantite;
+  pathImage;
 
-    constructor(id, name, prix, quantite, pathImage) {
-        this.id = id; 
-        this.name = name; 
-        this.prix = prix; 
-        this.quantite = quantite; 
-        this.pathImage = pathImage; 
-    }
-
+  constructor(id, name, prix, quantite, pathImage) {
+    this.id = id;
+    this.name = name;
+    this.prix = prix;
+    this.quantite = quantite;
+    this.pathImage = pathImage;
+  }
 }
 
-let products = []; 
+let products = [];
 
+function addProductToPanier() {
+  getPanierfromLocalStorage();
+  //cherche le produit dans le localStorage
+  var productInfo = JSON.parse(localStorage.getItem("productInfo"));
+  let productDetail = new Product(
+    productInfo.id,
+    productInfo.name,
+    productInfo.prix,
+    parseInt(document.getElementById("quantity").value),
+    productInfo.pathImage
+  );
 
-function addProductToPanier () {
-    getPanierfromLocalStorage(); 
-    //cherche le produit dans le localStorage 
-    var productInfo = JSON.parse(localStorage.getItem("productInfo"));
-    let productDetail = new Product(productInfo.id, productInfo.name,
-                                    productInfo.prix, 
-                                    parseInt(document.getElementById("quantity").value),
-                                    productInfo.pathImage); 
-     console.log("ajout de ce produit " + productDetail.JSON); 
-     
-     //ajout à l'array 
-     products.push(productDetail); 
-     addPanierToLocalStorage(); 
-     console.log("products apres ajout " + products.length); 
+  //ajout à l'array
+  products.push(productDetail);
+  addPanierToLocalStorage();
 }
 
 function addPanierToLocalStorage() {
-
-    localStorage.setItem("panier", JSON.stringify(products)); 
+  localStorage.setItem("panier", JSON.stringify(products));
 }
 
 function getPanierfromLocalStorage() {
-    products = JSON.parse(localStorage.getItem("panier")) || [];
-
+  products = JSON.parse(localStorage.getItem("panier")) || [];
 }
 
 function getPanierContent() {
 
-    console.log(typeof products); // Vérifie le type
-console.log(Array.isArray(products)); // Vérifie si c'est un tableau
-console.log(products); // Affiche son contenu
+  //containerDetail
+  const contentPanier = document.getElementById("productFromPanier");
+  products = JSON.parse(localStorage.getItem("panier")) || [];
 
+  products.forEach((product) => {
+    //creation de la ligne
+    const line = document.createElement("div");
+    line.className = "lineProduct";
 
-//containerDetail
-    const contentPanier = document.getElementById("productFromPanier")
-    console.log("products  " + products.length); 
-     products = JSON.parse(localStorage.getItem("panier")) || [];
+    //image
+    const divImage = document.createElement("div");
+    divImage.className = "contentImgProduct";
+    divImage.innerHTML = "";
 
-    console.log("products second " + products.length);
+    const imgProduct = document.createElement("img");
+    imgProduct.className = "imgLineProduct";
+    imgProduct.src = product.pathImage;
+
+    divImage.appendChild(imgProduct);
+    line.appendChild(divImage);
+
+    //titre & quantite
+    const contentProduct = document.createElement("div");
+
+    //title
+    const titleProduct = document.createElement("div");
+    titleProduct.className = "titleProduct";
+
+    titleProduct.innerHTML = product.name;
+
+    contentProduct.appendChild(titleProduct);
+
+    //Quantity
+    const contentQuantity = document.createElement("div");
+    contentQuantity.className = "number-input";
+
+    const btnDecrease = document.createElement("button");
+    btnDecrease.className = "button-number decrease";
+    btnDecrease.innerHTML = "-";
+
+    const btnIncrease = document.createElement("button");
+    btnIncrease.id = "increase";
+    btnIncrease.className = "button-number increase";
+    btnIncrease.innerHTML = "+";
+
+    btnDecrease.addEventListener("click", () => {
+      console.log("decrease button"); 
+      let currentValue = parseInt(numberInput.value);
+      if (currentValue > parseInt(numberInput.min)) {
+        numberInput.value = currentValue - 1;
+      }
+    });
+
+    btnIncrease.addEventListener("click", () => {
+      console.log("increase button"); 
+      let currentValue = parseInt(numberInput.value);
+      if (currentValue < parseInt(numberInput.max)) {
+        numberInput.value = currentValue + 1;
+      }
+    });
+
+    const numberInput = document.createElement("input");
+    numberInput.className = "quantityNumber";
+    numberInput.type = numberInput;
+    numberInput.value = product.quantite;
+    numberInput.min = 0;
+    numberInput.max = 100;
+
+    contentQuantity.appendChild(btnDecrease);
+    contentQuantity.appendChild(numberInput);
+    contentQuantity.appendChild(btnIncrease);
+
+    contentProduct.appendChild(contentQuantity);
+
+    line.appendChild(contentProduct);
+
+    //prix & croix de suppression 
+    const contentRight = document.createElement("div"); 
     
-
-   products.forEach(product => {
-
-    const title = document.createElement("div"); 
-    title.className = "titleProduct" ; 
+    const croixSuppression = document.createElement("button"); 
+    croixSuppression.className =  "croixSuppression"; 
+    croixSuppression.innerHTML = "&times";
     
+    croixSuppression.onclick  = () => deleteArticleFromPanier(product.id); 
 
-  
+    const prix = document.createElement("div");
+    prix.innerHTML = product.prix + "€";
 
-    title.innerHTML = product.name +  "  / quantity " + product.quantite; 
+    contentRight.appendChild(croixSuppression); 
+    contentRight.appendChild(prix); 
 
-    contentPanier.appendChild(title); 
- 
-   });
+    line.appendChild(contentRight);
+
+    contentPanier.appendChild(line);
+  });
+}
+
+function deleteArticleFromPanier(id) {
+  console.log("Suppression de l'article avec l'id : " + id);
+
+  // Filtrer le produit à supprimer
+  products = products.filter((product) => product.id !== id);
+
+  // Met à jour le localStorage
+  addPanierToLocalStorage();
+
+  // Met à jour l'affichage du panier
+  deletePanierContent();
+  getPanierContent();
 
 }
 
 function deletePanierContent() {
-    const contentPanier = document.getElementById("productFromPanier");
-    contentPanier.innerHTML = ""; 
+  const contentPanier = document.getElementById("productFromPanier");
+  contentPanier.innerHTML = "";
 }
